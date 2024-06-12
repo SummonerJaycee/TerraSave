3f656eeca03952908133bb5087e23753e4031178
summoner jaycee <summonerjaycee@proton.me>
2024-06-12 14:32:02 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Bounce button
+    $("#animatebutton").click(function(){
+    const element = document.querySelector('.animatebutton');
+    element.classList.add('animated', 'rubberBand');
+    setTimeout(function() {
+    element.classList.remove('rubberBand');
+    }, 2000);
+    });
+    
+    
+    });
